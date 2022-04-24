a6e7e3b430b5924434578ec23ae9896254872168
WojciechCebula <wojtekcebulaa@gmail.com>
2022-04-25 00:02:33 +0200

    Added stub file for Intellisense

---
+++ cython/chess.pyi
@@ -0,0 +1,24 @@
+
+class Coordinates:
+    row: int
+    column: int
+
+    def __init__(self, row: int = None, column: int = None) -> None:
+        ...
+
+
+class Move:
+    origin: Coordinates
+    destination: Coordinates
+    promotion: int
+
+    def __init__(self, origin: Coordinates, destination: Coordinates, promotion: int = 0) -> None:
+        ...
+
+
+class Board:
+    def __init__(self, fen: bytes) -> None:
+        ...
+
+    def make_move(self, move: Move) -> None:
+        ...
