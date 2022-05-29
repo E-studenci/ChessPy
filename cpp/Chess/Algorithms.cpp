1294261802f69a689d2f8ee431cf36ed963ef310
JakubMakarewicz <makaron@KOMPUTER>
2022-05-29 21:46:47 +0200

    saf

--- cpp/Chess/Algorithms.cpp
+++ cpp/Chess/Algorithms.cpp
@@ -43,92 +43,25 @@ std::tuple<int, std::vector<std::tuple<Move, int>>> Algorithms::Perft(Board *boa
 	return result;
 }
 
-void Algorithms::Worker(SafeQueue<Board *> &queue, std::atomic<int> &result, int depth)
-{
-	//while (!queue.isEmpty())
-	//{
-	//	Board *currentTask = queue.dequeue();
-	//	result += Perft(currentTask, depth);
-	//}
-}
-
-
-
-
-std::string Algorithms::PerftStarter(Board *board, int depth, bool divide)
-{
-	/*SafeQueue<Board *> tasks;
-	std::atomic<int> result = 0;*/
-
-	/*std::map<Coordinates, std::vector<Move>> currentLegalMoves = board->GetAllLegalMoves();
-	for (const std::pair<const Coordinates, std::vector<Move>> &keyValuePair : currentLegalMoves)
-	{
-		for (const auto &move : keyValuePair.second)
-		{
-			board->MakeMove(move);
-			Board *newBoard = new Board(*(board));
-			tasks.enqueue(newBoard);
-			board->Pop();
-		}
-	}
-	int threadCount = std::thread::hardware_concurrency() -1;
-	std::vector<std::thread> threads;
-	for (int i = 0; i < threadCount; i++)
-	{
-		std::thread t(Algorithms::Worker, std::ref(tasks), std::ref(result), depth);
-		threads.push_back(std::move(t));
-	}
-	for (int i = 0; i < threadCount; i++)
-	{
-		threads[i].join();
-	}*/
-	return "";
-}
-
-std::string Algorithms::PerftStarterSingleThread(Board *board, int depth, bool divide) {
+int Algorithms::PerftStarterSingleThread(Board *board, int depth, bool divide) {
 	this->count = 0;
 	std::string retString;
 	std::tuple<int, std::vector<std::tuple<Move, int>>> result = this->Perft(board, depth, divide);
-	retString += "Result: " + std::to_string(std::get<0>(result));
-	if (divide) {
-		retString += "\nDivide: \n";
-		for (int moveIndex = 0; moveIndex < std::get<1>(result).size()-1; moveIndex++) {
-			retString += "\t";
-			retString += std::get<0>(std::get<1>(result)[moveIndex]).ToString() + ": ";
-			retString += std::to_string(std::get<1>(std::get<1>(result)[moveIndex]));
-			retString += "\n";
-		}
-		retString += "\t";
-		retString += std::get<0>(std::get<1>(result)[std::get<1>(result).size() - 1]).ToString() + ": ";
-		retString += std::to_string(std::get<1>(std::get<1>(result)[std::get<1>(result).size() - 1]));
-	}
-	return retString;
-}
-
-std::pair<Move, int> Algorithms::GetBestMove(Board* board, int depth)
-{
-	this->count = 0;
-	//std::pair<Move, double> bestMove{ Move{Coordinates{-1,-1}, Coordinates{-1,-1}}, std::numeric_limits<double>::infinity() * (board->sideToMove? -1:1)};
-	std::pair<Move, int> bestMove{ Move{Coordinates{-1,-1}, Coordinates{-1,-1}}, -std::numeric_limits<double>::infinity() };
-
-	std::map<Coordinates, std::vector<Move>> currentLegalMoves = board->GetAllLegalMoves();
-	for (const std::pair<const Coordinates, std::vector<Move>>& keyValuePair : currentLegalMoves)
-	{
-		for (const Move& move : keyValuePair.second)
-		{
-			board->MakeMove(move);
-			int moveScore = this->AlphaBeta(board, -std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity(), depth);
-			board->Pop();
-			//if (!board->sideToMove)
-			//	moveScore *= -1;
-			//if ((!board->sideToMove && (moveScore < bestMove.second)) || (board->sideToMove && (moveScore > bestMove.second))) {
-			if (moveScore > bestMove.second) {
-				bestMove.first = move; // TODO: this probably doesn't work
-				bestMove.second = moveScore;
-			}
-		}
-	}
-	return bestMove;
+	return std::get<0>(result);
+	//retString += "Result: " + std::to_string(std::get<0>(result));
+	//if (divide) {
+	//	retString += "\nDivide: \n";
+	//	for (int moveIndex = 0; moveIndex < std::get<1>(result).size()-1; moveIndex++) {
+	//		retString += "\t";
+	//		retString += std::get<0>(std::get<1>(result)[moveIndex]).ToString() + ": ";
+	//		retString += std::to_string(std::get<1>(std::get<1>(result)[moveIndex]));
+	//		retString += "\n";
+	//	}
+	//	retString += "\t";
+	//	retString += std::get<0>(std::get<1>(result)[std::get<1>(result).size() - 1]).ToString() + ": ";
+	//	retString += std::to_string(std::get<1>(std::get<1>(result)[std::get<1>(result).size() - 1]));
+	//}
+	//return retString;
 }
 
 std::pair<Move, int> Algorithms::Root(Board* board, int max_depth, long timeInMillis)
