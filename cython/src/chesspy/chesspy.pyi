c4ce9ea222dc0193cada4ef73d65569b3fad7e02
WojciechCebula <wojtekcebulaa@gmail.com>
2022-06-18 16:33:32 +0200

    Environment and basic modules for drl

--- cython/src/chesspy/chesspy.pyi
+++ cython/src/chesspy/chesspy.pyi
@@ -5,18 +5,16 @@ class Coordinates:
     row: int
     column: int
 
-    def __init__(self, row: int = None, column: int = None) -> None:
-        ...
-
+    def __init__(self, row: int = None, column: int = None) -> None: ...
 
 class Move:
     origin: Coordinates
     destination: Coordinates
     promotion: int
 
-    def __init__(self, origin: Coordinates, destination: Coordinates, promotion: int = 0) -> None:
-        ...
-
+    def __init__(
+        self, origin: Coordinates, destination: Coordinates, promotion: int = 0
+    ) -> None: ...
 
 class Board:
     board: list[list[int]]
@@ -25,19 +23,11 @@ class Board:
     three_fold_repetition: bool
     fifty_move_rule_draw: bool
     fen: str
-    
-    def __init__(self, fen: bytes) -> None:
-        ...
 
-    def make_move(self, move: Move) -> None:
-        ...
-        
-    def pop_move(self) -> None:
-        ...
-    
-    def get_all_legal_moves(self) -> dict[Coordinates, list[Move]]:
-        ...
-        
+    def __init__(self, fen: bytes) -> None: ...
+    def make_move(self, move: Move) -> None: ...
+    def pop_move(self) -> None: ...
+    def get_all_legal_moves(self) -> dict[Coordinates, list[Move]]: ...
 
 class SearchResult:
     evaluation: int
@@ -45,22 +35,23 @@ class SearchResult:
     reached_depth: int
     best_move: Move
     best_opponent_move: Move
-
+    node_count: list[int]
 
 class MoveOrderingType(enum.Enum):
     HANDCRAFTED = 0
     TRAINING = 1
     MODEL = 2
 
-
 class SearchEngine:
     count: int
-    
-    def __init__(self, move_ordering_type: MoveOrderingType = None) -> None:
-        ...
 
-    def perft(self, board: Board, depth: int) -> int:
-        ...
-    
-    def root(self, board: Board, depth: int, time_in_milliseconds: int, evaluatePosition = False, getOpponentBestMove = False) -> EvaluationResult:
-        ...
+    def __init__(self, move_ordering_type: MoveOrderingType = None) -> None: ...
+    def perft(self, board: Board, depth: int) -> int: ...
+    def root(
+        self,
+        board: Board,
+        depth: int,
+        time_in_milliseconds: int,
+        evaluatePosition=False,
+        getOpponentBestMove=False,
+    ) -> SearchResult: ...
--- deeplearning/main.py
+++ deeplearning/main.py
@@ -1,15 +1,28 @@
 import os
 
 import hydra
+import chesspy
 
-@hydra.main(config_path='config', config_name='default.yaml')
+import reinforcement.environment as env
+
+
+@hydra.main(config_path="config", config_name="default.yaml", version_base=None)
 def main(config):
-    pass
+    for board in env.GameProvider(config.data_path):
+        engine = chesspy.SearchEngine(0)
+        for result in env.GameManager(
+            board,
+            engine,
+            config.max_game_length,
+            config.max_depth,
+            config.max_move_time,
+        ):
+            print(board.fen, result.node_count)
 
 
 def evaluate_move(board, move):
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
